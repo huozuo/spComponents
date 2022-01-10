70f3702961030229b5171091cd1a326febd8087a
huozuo <1163013693@qq.com>
2022-01-10 15:00:43 +0800

    update demo for eays use, add some annotation

--- demo.py
+++ demo.py
@@ -8,10 +8,11 @@ func: 用于运行spComponents下的跑py
 import spComponents
 
 if __name__=="__main__":
-    spComponents.sparseRepresentation.sample.run(10)
-    spComponents.MBF.runMBF.run()
-    spComponents.sparseRepresentation.atomGen.run()
-    spComponents.sparseRepresentation.atom2nodes.run()
+    spComponents.sparseRepresentation.sample.run(10)    #采样
+    spComponents.sparseRepresentation.ksvd.run() #浮点矩阵分解 ksvd （与布尔矩阵二选一）
+    # spComponents.MBF.runMBF.run()     #布尔矩阵分解
+    spComponents.sparseRepresentation.atomGen.run()     #原子生成，恢复网络
+    # spComponents.sparseRepresentation.atom2nodes.run()    #原子--实际节点
     spComponents.sparseRepresentation.plotAtoms.run() # 绘制原子图像
 
 
