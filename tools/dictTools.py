f709531cb7b677c1f1dda2c7290468031b43865d
huozuo <1163013693@qq.com>
2022-02-04 11:01:28 +0800

    更新dictTools功能

--- tools/dictTools.py
+++ tools/dictTools.py
@@ -5,8 +5,10 @@ description: 适用于dict的tools
 '''
 
 
-def reverseDict(dict):
+def reverseValList(dict):
     '''
+    翻转DICT
+    val值 默认 list
     将Dict的key value交换，返回仍是Dict
     return {v:k for k,v in dict.items()} # 只能解决value为单个变量的情况
     :param dict: Dict形式{k:[v1,v2,v3]}
@@ -24,4 +26,18 @@ def reverseDict(dict):
             newDict[vs] = [k]
 
     return newDict
-                    
+
+def reverse(dict):
+    '''
+    翻转DICT
+    val值不含list
+    :return: {v:[k1,k2]}
+    '''
+    newDict = {}
+    for k,v in dict.items():
+        if v in newDict.keys():
+            newDict[v].append(k)
+        else:
+            newDict[v] = [k]
+    return newDict
+
